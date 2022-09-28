ae462a9d099ce595e8d998cb60bcf2f92e9c7958
jibril <jazimjibril@gmail.com>
2022-09-28 11:34:49 +0300

    added tailwindCSS ✔️

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <nav class="w-f">sssss</nav>
+    <nav class="w-full border border-red-400 h-20 flex justify-around items-center">
+      <h1>Where in the world?</h1>
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Dark Mode
+      </button>
+    </nav>
   </div>
 </template>
 

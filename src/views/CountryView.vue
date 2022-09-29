a050ebaa081e9f9e3680628e44a846f0f81274d3
jibril <jazimjibril@gmail.com>
2022-09-29 18:33:47 +0300

    made changes ⚡

--- src/views/CountryView.vue
+++ src/views/CountryView.vue
@@ -2,14 +2,14 @@
   <div class="px-40 py-20 m-auto h-[100vh]">
     <router-link
       to="/"
-      class="border inline-flex items-center px-7 py-1 shadow-xl rounded-md"
+      class="border inline-flex items-center px-7 py-1 shadow-xl rounded-md dark:text-white"
     >
       <i class="fa-solid fa-arrow-left mr-3"></i>
       <p>Back</p>
     </router-link>
 
     <!-- country details -->
-    <div class="flex h-full items-center">
+    <div class="flex h-5/6 items-center">
       <div class="w-1/2">
         <img
           class="lg:h-[45vh] object-contain object-center w-full ml"
@@ -18,34 +18,45 @@
         />
       </div>
 
-      <div class="w-1/2 flex flex-col gap-10 pl-24">
-        <h1 class="text-4xl font-bold">{{ country.name }}</h1>
+      <div class="w-1/2 flex flex-col gap-10 pl-24 dark:text-white">
+        <h1 class="text-4xl font-bold dark:text-stone-400">
+          {{ country.name }}
+        </h1>
         <div class="flex justify-between">
           <div class="mr-10 w-1/2 flex flex-col gap-5">
             <h2>
-              <span class="font-bold">Native Name:</span>
+              <span class="font-bold dark:text-stone-400">Native Name:</span>
               {{ country.nativeName }}
             </h2>
             <h2>
-              <span class="font-bold">Population:</span>
+              <span class="font-bold dark:text-stone-400">Population:</span>
               {{ country.population.toLocaleString() }}
             </h2>
-            <h2><span class="font-bold">Region:</span> {{ country.region }}</h2>
-            <h2><span class="font-bold">Sub Region:</span> {{ country.subregion }}</h2>
-            <h2><span class="font-bold">Capital:</span> {{ country.capital }}</h2>
+            <h2>
+              <span class="font-bold dark:text-stone-400">Region:</span>
+              {{ country.region }}
+            </h2>
+            <h2>
+              <span class="font-bold dark:text-stone-400">Sub Region:</span>
+              {{ country.subregion }}
+            </h2>
+            <h2>
+              <span class="font-bold dark:text-stone-400">Capital:</span>
+              {{ country.capital }}
+            </h2>
           </div>
           <div class="w-1/2 mr-10 flex flex-col gap-5">
             <h2>
-              <span class="font-bold">Top Level Domain:</span>
+              <span class="font-bold dark:text-stone-400">Top Level Domain:</span>
               {{ country.topLevelDomain[0] }}
             </h2>
             <h2>
-              <span class="font-bold">Currencies:</span>
+              <span class="font-bold dark:text-stone-400">Currencies:</span>
               {{ country.currencies[0].name }}
             </h2>
             <!-- languages -->
             <h2>
-              <span class="font-bold">Languages:</span>
+              <span class="font-bold dark:text-stone-400">Languages:</span>
               <span v-for="(language, index) in country.languages" :key="index">
                 {{ language.name }}
                 <span v-if="index < country.languages.length - 1">,</span>
@@ -54,7 +65,7 @@
           </div>
         </div>
         <div>
-          <span class="font-bold pr-1">Border Countries:</span>
+          <span class="font-bold dark:text-stone-400 pr-1">Border Countries:</span>
           <!-- border countries -->
 
           <router-link
